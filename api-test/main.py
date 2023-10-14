494a1e3f51df56b700e0d23e8b874fa785ddb269
Justin Bax <justin.bax@icloud.com>
2023-10-14 14:46:06 -0400

    basic api

--- api-test/main.py
+++ api-test/main.py
@@ -1,15 +1,53 @@
 import datetime as dt
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
-permissions = ['Access to very important files', 'Access to authenticated files', 'Access to base files']
+permissions = {
+    'lvl0': ['/balls.txt', '/btw.txt'],
+    'lvl1': ['/btw.txt'],
+    'lvl2': []
+}
 
+users = {
+    'joe@mama.com': 'lvl0',
+    'mama@joe.com': 'lvl1',
+    'chris@btw.com': 'lvl2'
+}
+
+def get_unique_name(uuid):
+    # TODO this
+    return 'chris@btw.com'
 
 @app.route('/permissions')
 def get_permissions():
     return jsonify(permissions)
 
-@app.route('requests', methods=['POST'])
+# Template response JSON
+# {
+#   'status': "GRANTED" or "DENIED",
+# }
+
+@app.route('/requests', methods=['GET'])
 def receive_requests():
-    return
+    # Identify permission level required to get file
+    uuid = request.args.get('uuid')
+    resource = request.args.get('resource')
+
+    unique_name = get_unique_name(uuid)
+
+    if unique_name in users:
+        perms = users[unique_name]
+    else:
+        return jsonify({'status': 'INVALID'})
+
+    response = {
+        'status': 'DENIED'
+    }
+    # Verifies permission level
+    if resource in permissions[perms]:
+        # yay
+        response['status'] = 'GRANTED'
+
+    return jsonify(response)
